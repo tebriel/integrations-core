fe03764ae6fec2dd8c099a73498554931a335457
Greg Meyer <greg.meyer@datadoghq.com>
2018-10-25 12:11:26 -0400

    more

--- ibm_mq/datadog_checks/ibm_mq/mqsc.py
+++ ibm_mq/datadog_checks/ibm_mq/mqsc.py
@@ -63,7 +63,14 @@ def run_mqsc_cmd(cmd,
 
     log.warning("local subproc: {}".format(result))
 
-    log.warning("directly calling popen: {}".format(subprocess.Popen(command)))
+    with tempfile.TemporaryFile() as stdout_f, tempfile.TemporaryFile() as stderr_f:
+        proc = subprocess.Popen(command, stdout=stdout_f, stderr=stderr_f)
+        proc.wait()
+        stderr_f.seek(0)
+        err = stderr_f.read()
+        stdout_f.seek(0)
+        output = stdout_f.read()
+    log.warning("directly calling popen: output: {} error: {}".format(output, err))
 
     result = get_subprocess_output(test_command, log, raise_on_empty_output=False)
 
